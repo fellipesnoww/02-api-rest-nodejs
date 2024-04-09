04080cf1802c266df9098c0b35d25878f88bd554
Fellipe <fellipe2.0e@gmail.com>
2024-04-09 11:08:53 -0300

    chore: created new migrations

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import fastify from "fastify";
 import { knex } from "./database";
+import crypto from "node:crypto";
 
 const app = fastify();
 
 app.get("/hello", async (req, reply) => {
-  const tables = await knex('sqlite_schema').select('*')
+  // const transaction = await knex('transactions').insert({
+  //   id: crypto.randomUUID(),
+  //   title: 'Transação de teste',
+  //   amount: 1000,
+  // }).returning('*')
 
-  return tables;
+  const transaction = await knex('transactions').select('*')
+
+  return transaction;
 });
 
 app.listen({
